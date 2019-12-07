ce55016cb94e1c1f81498468278274b52d3713e5
kang.ruibo <kang.ruibo@oyohotels.cn>
2019-12-07 13:39:31 +0800

    story加入note

--- React/components/stories/demo.stories.js
+++ React/components/stories/demo.stories.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Demo from '../DemoComponent/index';
+import note from './note.md';
 
-storiesOf('Demo', module).add('with data', () => <Demo />);
+storiesOf('Demo', module).add('样例', () => <Demo />, {
+  notes: note
+});
