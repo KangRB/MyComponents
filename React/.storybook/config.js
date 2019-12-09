e72dd3cb12fc4ac0f0a9bc9a35e29952dcd214ca
kang.ruibo <kang.ruibo@oyohotels.cn>
2019-12-09 15:16:56 +0800

    增加viewport插件

--- React/.storybook/config.js
+++ React/.storybook/config.js
@@ -1,6 +1,12 @@
-import { configure } from '@storybook/react';
-
+import { configure, addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import '@storybook/addon-console';
 // automatically import all files ending in *.stories.js
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+});
 configure(
   require.context('../components/stories', true, /\.stories\.js$/),
   module
--- React/components/stories/demo.stories.js
+++ React/components/stories/demo.stories.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Demo from '../DemoComponent/index';
 import note from './note.md';
+import { action } from '@storybook/addon-actions';
 
-storiesOf('Demo', module).add('样例', () => <Demo title={'props'} />, {
-  notes: note
-});
+storiesOf('Demo', module).add(
+  '样例',
+  () => <Demo onClick={action('clicked')} title={'props'} />,
+  {
+    notes: note
+  }
+);
